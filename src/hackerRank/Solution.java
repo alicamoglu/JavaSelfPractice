6833bc4d35ee20c26d427910aeae03febe2dd646
Ali Camoglu <119692910+alicamoglu@users.noreply.github.com>
2023-05-14 10:02:01 +0200

    adding practice change

---
+++ src/hackerRank/Solution.java
@@ -0,0 +1,21 @@
+package hackerRank;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String aa = scan.next();
+        int a = scan.nextInt();
+        scan.close();
+
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        // Complete this line
+        // Complete this line
+        System.out.println("myString is: " + aa);
+        System.out.println("myInt is: " + a);
+    }
+}
